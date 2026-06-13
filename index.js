cf924d7e7c997d08def328fccbd7b6d6e4d582b7
Zihad <dev.zihad007@gmail.com>
2026-06-13 13:46:22 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+//for (let i = 10; i >= 1; i--) {
+  //  console.log(i);
+//}
+     //Loop
+//let number = 6;
+
+//for (let i = 1; i <=10; i++) {
+  //  console.log(number + " x " + i + " = " + number * i);
+//}
+    // While loop
+
+//let i = 1;
+  
+//while (i <= 5) {
+//console.log(i);
+//i++
+//}
+
+//let Gangsters = ["Arthar", "Tommy", "Jhon"];
+
+//for (let i = 0; i < Gangsters.length; i++) {
+ // console.log(Gangsters[i]);
+//}
+
+//for (let i = 2; i <=20;i +=2 ){
+//console.log(i);
+//}
+       //function Return
+//function add(a, b) {
+ // return a + b;
+//}
+
+//let result = add(49, 1);
+//console.log(result);
+
+       //Arrow Function
+  //const greet = (name) => {
+  //  return "Only" + name;
+  //};
+         //array Push
+ // let name = ["Zihad", "Mahi", ];
+
+ // name.push("Nafiz",);
+
+ // console.log(name)
+          //Array pop
+  let name = ["Zihad", "Mahi", ];
+  
+     name.pop("Mahi");
+
+     console.log(name)
+
+        
